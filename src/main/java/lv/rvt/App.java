4bb8788e3a211f92631003d46d7ba5a47fbadcc4
VadimsMalickis <vadims.malickis@rvt.lv>
2024-11-14 14:41:43 +0200

    changing template

--- src/main/java/lv/rvt/App.java
+++
@@ -1,9 +0,0 @@
-package lv.rvt;
-
-public class App 
-{
-    public static void main( String[] args )
-    {
-        // Make your Hello World programm!
-    }
-}
---
+++ src/main/java/lv/rvt/Main.java
@@ -0,0 +1,16 @@
+package lv.rvt;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.StandardOpenOption;
+
+import lv.rvt.tools.Utils;
+
+public class Main 
+{
+    public static void main( String[] args )
+    {
+        // Make your first Hello World!
+    }
+}
